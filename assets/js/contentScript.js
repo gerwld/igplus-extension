7e31b0f2259f8db3290a194249b62797709f1a42
P Jaworski <pjaworski.dev@gmail.com>
2023-07-11 21:53:36 +0200

    Added license

--- assets/js/contentScript.js
+++ assets/js/contentScript.js
@@ -1,3 +1,18 @@
+//   - This file is part of InstaPlus Extension
+//  <https://github.com/gerwld/InstaPlus-extension/blob/main/README.md>,
+//   - Copyright (C) 2019-present InstaPlus Extension, Inc.
+//   -
+//   -InstaPlus Extension is free software: you can redistribute it and/or modify
+//   - it under the terms of the Creative Commons Attribution-NonCommercial 4.0 International (CC BY-NC 4.0) License.
+//   -
+//   -InstaPlus Extension is distributed in the hope that it will be useful,
+//   - but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   - MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   - Creative Commons Attribution-NonCommercial 4.0 International (CC BY-NC 4.0) License for more details.
+//   -
+//   - You should have received a copy of the Creative Commons Attribution-NonCommercial 4.0 International (CC BY-NC 4.0) License
+//   - along withInstaPlus Extension.  If not, see <https://creativecommons.org/licenses/by-nc/4.0/>.
+
 let interval0, interval1, interval2, interval3, interval4, interval5, interval6;
 const fonts = ["roboto", "poppins", "caprasimo", "playfair", "merriweather", "noto_sans"];
 const themes = ["light_green", "purple_dark", "kittens"];
