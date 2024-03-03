8b1afb842b0845e464b29f96d099d69127c0d426
P Jaworski <pjaworski.dev@gmail.com>
2024-03-03 21:38:42 +0100

    build: updated rate popup

--- assets/js/content.js
+++ assets/js/content.js
@@ -314,10 +314,10 @@
   "use strict";
   (() => {
     const APPEAR_TIMEOUT = 10 * 1000 * 60;
-    // const APPEAR_TIMEOUT = 4000;
+    // const APPEAR_TIMEOUT = 1000;
     const MAX_CLOSE_COUNT = 5;
     const browser_cr = chrome ? chrome : browser;
-    const store_links = {
+    const STORE_LINKS = {
       "chrome": "https://chromewebstore.google.com/detail/dbbopjndlaginbghfoibbndhlbpdpapd/reviews/write",
       "firefox": "https://addons.mozilla.org/en-US/firefox/addon/igplus-extension/reviews/",
       "edge": "https://microsoftedge.microsoft.com/addons/detail/igplus-remove-instagram/gcjgjfjabmgpainpahloaldflhfnppai",
@@ -338,7 +338,7 @@
     const initRateMePopup = () => {
       const browser = detectBrowser();
 
-      if (browser && store_links[browser]) {
+      if (browser && STORE_LINKS[browser]) {
         browser_cr.storage.local.get('closeCount', function (data) {
 
           if (!data.closeCount) {
@@ -348,8 +348,121 @@
           if (!data.closeCount || data.closeCount < MAX_CLOSE_COUNT) {
             const notification = document.createElement('div');
             const logo = browser_cr.runtime.getURL('assets/img/logo.svg');
-            notification.setAttribute('id', "ext_show");
-            notification.innerHTML = `<div> <div class="groupl"> ${logo ? `<img src="${logo}" alt="logo"/>` : ''} <div> <h1>It would really help!</h1> </div></div> <p>If you enjoy using my extension, would you mind rate it on the webstore, then?</p> <a href="${store_links[browser]}" target="_blank" id="rateLink">Rate it</a> <div class="cls"> <span id="closeNotification" style="cursor: pointer;">No, Thanks</span> </div> </div> <style id="43ggfdbt5rf"> #ext_show img, #ext_show p { user-select: none; pointer-events: none; } #ext_show h1 { width: 100%; display: block; text-align: left; color: #ffffff!important; font-weight: 600; font-size: 20px; } #ext_show .groupl { display: flex; align-items: center; justify-content: center; } #ext_show h1.first { margin-bottom: 5px; } #ext_show p { max-width: 290px; font-size: 14px; font-weight: 400; margin: 8px 0 16px; color: #868b90!important; line-height: 140%; text-align: center; } #ext_show a { display: block; border: 1px solid rgb(68, 86, 91, 0.5); border-radius: 10px; padding: 6px 10px; margin: 10px auto; max-width: 270px; background-color: rgba(255,255,255, 0.16)!important; color: white!important; text-align: center; } #ext_show a:hover { text-decoration: none; background-color: rgba(255,255,255, 0.1)!important; } #ext_show a:focus { text-decoration: none; } #ext_show > div { font-family: "Inter", inherit, serif; width: 296px; position: fixed; top: 10px; right: 10px; background-color: #161515!important; padding: 10px 8px 9px; border: 1px solid rgb(68, 86, 91, 0.5); z-index: 100; border-radius: 12px } #ext_show img { margin-right: 10px; margin-left: -10px; height: 33px; width: auto; max-width: 40px; box-shadow: 0 2px 2px 2px rgb(33, 33, 30, 0.15); } #ext_show .cls { display: flex; justify-content: center; } #closeNotification { display: inline-block; margin: 0 auto; padding-left: 4px; text-align: center; font-size: 11px; color: #72767a!important; } #closeNotification:hover { text-decoration: underline; } </style> `;
+            notification.innerHTML = `
+            <div id="ext_show"><div><div class="groupl">${logo ? `<img src = "${logo}" alt = "logo" /> ` : ''}
+            <h1>It would really help.</h1></div><p>If you enjoy using this extension,
+            would you mind rate it on the webstore,
+            then?</p><a href="${STORE_LINKS[browser]}" target="_blank" id="rateLink" data-action="RATE">Rate it</a><div class="cls"><span id="closeNotification" data-action="CLOSE" style="cursor: pointer;">No,
+            Thanks</span></div></div></div><style id="43ggfdbt5rf">#ext_show img,
+            #ext_show p {
+              user-select: none;
+              pointer-events: none;
+            }
+      
+            #ext_show h1 {
+              display: block;
+              text-align: left;
+              color: #ffffff !important;
+              font-weight: 600;
+              font-weight: 500;
+              font-size: 21px;
+              line-height: 21px;
+              margin: 0;
+            }
+      
+            #ext_show .groupl {
+              display: flex;
+              align-items: center;
+              justify-content: center;
+              margin: 10px 0 10px -5px;
+            }
+      
+            #ext_show h1.first {
+              margin-bottom: 5px;
+            }
+      
+            #ext_show p {
+              max-width: 290px;
+              font-size: 14px;
+              font-size: 12.8px;
+              font-weight: 400;
+              margin: 8px 0 16px;
+              color: #868b90 !important;
+              line-height: 140%;
+              text-align: center;
+            }
+      
+            #ext_show a {
+              text-decoration: none !important;
+              display: block;
+              border: 1px solid rgb(68, 86, 91, 0.5);
+              border-radius: 22px;
+              padding: 7px 10px;
+              margin: 10px auto;
+              max-width: 270px;
+              background-color: rgba(255, 255, 255, 0.16) !important;
+              color: white !important;
+              text-align: center;
+              font-size: 14px;
+              font-size: 14.5px;
+            }
+      
+            #ext_show a:hover {
+              text-decoration: none;
+              background-color: rgba(255, 255, 255, 0.1) !important;
+            }
+      
+            #ext_show a:focus {
+              text-decoration: none;
+            }
+      
+            #ext_show>div {
+              transform: scale(1);
+              box-shadow: rgba(0, 0, 0, 0.8) 0px 8px 24px;
+              z-index: 100000 !important;
+              font-family: "Inter", system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+              width: 296px;
+              position: fixed;
+              top: 10px;
+              right: 10px;
+              background-color: #161515 !important;
+              background-color: rgb(22, 21, 21, 0.96) !important;
+              padding: 5px 12px 8px;
+              box-sizing: border-box;
+              border: 1px solid rgb(68, 86, 91, 0.5);
+              z-index: 100;
+              border-radius: 12px
+            }
+      
+            #ext_show img {
+              margin-right: 10px;
+              height: 33px;
+              width: auto;
+              max-width: 40px;
+              box-shadow: 0 2px 2px 2px rgb(33, 33, 30, 0.15);
+            }
+      
+            #ext_show .cls {
+              display: flex;
+              justify-content: center;
+            }
+      
+            #closeNotification {
+              display: inline-block;
+              margin: 0 auto;
+              padding-left: 4px;
+              text-align: center;
+              font-size: 11px;
+              font-size: 10.5px;
+              color: #72767a !important;
+            }
+      
+            #closeNotification:hover {
+              text-decoration: underline;
+            }
+      
+            </style>
+              `;
 
             const appendPopup = () => {
               // Append the notification to the body
